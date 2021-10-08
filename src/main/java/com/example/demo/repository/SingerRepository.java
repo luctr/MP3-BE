1b1b6f32389841b1034c843db8c1c88d01e1f828
kien <kienk93@gmail.com>
2021-10-08 08:59:30 +0700

    pull

--- src/main/java/com/example/demo/repository/SingerRepository.java
+++ src/main/java/com/example/demo/repository/SingerRepository.java
@@ -3,6 +3,9 @@ import com.example.demo.model.Singer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SingerRepository extends JpaRepository<Singer,Long> {
+    List<Singer> findAllByNameContaining(String name);
 }
