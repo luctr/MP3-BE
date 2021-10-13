b100e4b1fe716c02ce344cd9723547212e14e15a
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-10-13 21:01:21 +0700

    commit

--- src/main/java/com/example/demo/service/songcategory/SongCategoryService.java
+++ src/main/java/com/example/demo/service/songcategory/SongCategoryService.java
@@ -1,12 +1,12 @@
 package com.example.demo.service.songcategory;
 
 import com.example.demo.model.SongCategory;
-import com.example.demo.model.User;
 import com.example.demo.repository.SongCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class SongCategoryService implements ISongCategoryService {
     @Autowired
     private SongCategoryRepository songCategoryRepository;
